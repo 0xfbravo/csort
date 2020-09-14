07732993105ba87e0cae786bc7775f279db0e72d
Fellipe Bravo <fellipe.bravo@gmail.com>
2020-09-14 18:35:58 -0300

    [Structure] Added base project

---
+++ cmake-build-debug/CMakeFiles/csort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csort.dir/main.c.o"
+  "csort"
+  "csort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
